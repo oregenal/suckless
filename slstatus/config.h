0c51bc6f56c3049437a607e261080fb85ca8f120
Andrey Gud <nosacred@gmail.com>
2022-07-28 17:34:47 +0700

    slstatus network monitoring ethernet

--- slstatus/config.h
+++ slstatus/config.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	//{ datetime, "%s",           "%F %T" },
-	{ netspeed_rx, "%sB/s ",        "wlp4s0"      },
+	{ netspeed_rx, "%sB/s ",        "enp3s0"      },
 	{ cpu_perc,    "[CPU %s%] ",    NULL          },
 	{ ram_perc,    "[RAM %s%] ",    NULL          },
 	{ keymap,      "[%s] ",         NULL          },
